33f6f29f6e075c5819441a09016f71355d781329
djzesi <dishantjain08@gmail.com>
2019-12-17 21:13:34 +0530

    Initial commit till routing

---
+++ src/app/products.ts
@@ -0,0 +1,17 @@
+export const products=[
+  {
+    name: 'Redmi 4',
+    price: '8500',
+    description: 'A medium budget phone with cool design'
+  },
+  {
+    name: 'Redmi Poco F1',
+    price: '100',
+    description: 'Gaming phone with best processor in world'
+  },
+  {
+    name: 'Redmi Note 8 Pro',
+    price: '16000',
+    description: 'Camera beast phone with 64 MP quad camera.'
+  }
+];
